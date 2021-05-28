dccf449efbe21b5e758e17e5634afdf73e89e491
Stepisk <user.ya0073@gmail.com>
2021-05-28 16:33:30 +0300

    Add sprite actor on field.

--- SFMLTestApp/SFMLTestApp.cpp
+++ SFMLTestApp/SFMLTestApp.cpp
@@ -3,7 +3,23 @@
 
 int main()
 {
-  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML Works!");
+  sf::RenderWindow window(sf::VideoMode(600, 600), "Field");
+
+  window.setFramerateLimit(65);
+
+  sf::Texture textureActor;
+  if (!textureActor.loadFromFile("../SFMLTestApp/Resources/actor.png"))
+    return -1;
+
+  sf::Sprite spriteActor;
+  spriteActor.setTexture(textureActor);
+
+  /*sf::Texture textureFPS;
+  if (!textureFPS.create(100, 100))
+    return -1;
+
+  sf::Sprite spriteFPS(textureFPS);*/
+
 
   // Главный цикл приложения. Выполняется, пока открыто окно
   while (window.isOpen())
@@ -20,9 +36,11 @@ int main()
 
     }
 
+    /*sf::Uint8* pixels = ;
+    textureFPS.update(pixels);
+    window.draw(spriteFPS);*/
 
-
-
+    window.draw(spriteActor);
     window.display();
   }
 
