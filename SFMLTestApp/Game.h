b7b1fe98549fab9fdadc59243b0e905cc21d03a3
Stepisk <user.ya0073@gmail.com>
2021-06-07 14:30:19 +0300

    cyan circle on field (41 page pdf)

---
+++ SFMLTestApp/Game.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class Game
+{
+public:
+  Game();
+  void run();
+private:
+  void processEvents();
+  void update(sf::Time deltaTime);
+  void render();
+  void handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
+private:
+  sf::RenderWindow mWindow;
+  sf::CircleShape mPlayer;
+  bool mIsMovingUp;
+  bool mIsMovingDown;
+  bool mIsMovingLeft;
+  bool mIsMovingRight;
+  float PlayerSpeed;
+  const sf::Time TimePerFrame = sf::seconds(1.f / 60.f);
+};
--- SFMLTestApp/SFMLTestApp.cpp
+++ SFMLTestApp/SFMLTestApp.cpp
@@ -1,75 +1,79 @@
 ﻿#include <iostream>
 #include <SFML/Graphics.hpp>
+#include "Game.h"
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(640, 480), "Field");
-	window.setFramerateLimit(65);
+	Game game;
+	game.run();
+
+	//sf::RenderWindow window(sf::VideoMode(640, 480), "Field");
+	//window.setFramerateLimit(65);
 
 
-	sf::Texture herotexture;
-	herotexture.loadFromFile("../SFMLTestApp/Resources/hero.png");
+	//sf::Texture herotexture;
+	//herotexture.loadFromFile("../SFMLTestApp/Resources/hero.png");
 
-	sf::Sprite herosprite;
-	herosprite.setTexture(herotexture);
-	herosprite.setTextureRect(sf::IntRect(0, 192, 96, 96));
-	herosprite.setPosition(250, 250);
+	//sf::Sprite herosprite;
+	//herosprite.setTexture(herotexture);
+	//herosprite.setTextureRect(sf::IntRect(0, 192, 96, 96));
+	//herosprite.setPosition(250, 250);
 
-	float CurrentFrame = 0;//хранит текущий кадр
-	sf::Clock clock;
+	//float CurrentFrame = 0;//хранит текущий кадр
+	//sf::Clock clock;
 
-	while (window.isOpen())
-	{
+	//while (window.isOpen())
+	//{
 
-		float time = clock.getElapsedTime().asMicroseconds();
-		clock.restart();
-		time = time / 800;
-
-
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
+	//	float time = clock.getElapsedTime().asMicroseconds();
+	//	clock.restart();
+	//	time = time / 800;
 
 
-		///////////////////////////////////////////Управление персонажем с анимацией////////////////////////////////////////////////////////////////////////
-		if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || (sf::Keyboard::isKeyPressed(sf::Keyboard::A)))) { //если нажата клавиша стрелка влево или англ буква А
-			CurrentFrame += 0.005 * time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
-			if (CurrentFrame > 3) CurrentFrame -= 3; //проходимся по кадрам с первого по третий включительно. если пришли к третьему кадру - откидываемся назад.
-			herosprite.setTextureRect(sf::IntRect(96 * int(CurrentFrame), 96, 96, 96)); //проходимся по координатам Х. получается 96,96*2,96*3 и опять 96
-			herosprite.move(-0.1 * time, 0);//происходит само движение персонажа влево
-		}
-
-		if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || (sf::Keyboard::isKeyPressed(sf::Keyboard::D)))) {
-			CurrentFrame += 0.005 * time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
-			if (CurrentFrame > 3) CurrentFrame -= 3; //проходимся по кадрам с первого по третий включительно. если пришли к третьему кадру - откидываемся назад.
-			herosprite.setTextureRect(sf::IntRect(96 * int(CurrentFrame), 192, 96, 96)); //проходимся по координатам Х. получается 96,96*2,96*3 и опять 96
-
-			herosprite.move(0.1 * time, 0);//происходит само движение персонажа вправо
-
-		}
+	//	sf::Event event;
+	//	while (window.pollEvent(event))
+	//	{
+	//		if (event.type == sf::Event::Closed)
+	//			window.close();
+	//	}
 
 
-		if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || (sf::Keyboard::isKeyPressed(sf::Keyboard::W)))) {
-			CurrentFrame += 0.005 * time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
-			if (CurrentFrame > 3) CurrentFrame -= 3; //проходимся по кадрам с первого по третий включительно. если пришли к третьему кадру - откидываемся назад.
-			herosprite.setTextureRect(sf::IntRect(96 * int(CurrentFrame), 288, 96, 96)); //проходимся по координатам Х. получается 96,96*2,96*3 и опять 96
-			herosprite.move(0, -0.1 * time);//происходит само движение персонажа вверх
-		}
+	//	///////////////////////////////////////////Управление персонажем с анимацией////////////////////////////////////////////////////////////////////////
+	//	if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || (sf::Keyboard::isKeyPressed(sf::Keyboard::A)))) { //если нажата клавиша стрелка влево или англ буква А
+	//		CurrentFrame += 0.005 * time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
+	//		if (CurrentFrame > 3) CurrentFrame -= 3; //проходимся по кадрам с первого по третий включительно. если пришли к третьему кадру - откидываемся назад.
+	//		herosprite.setTextureRect(sf::IntRect(96 * int(CurrentFrame), 96, 96, 96)); //проходимся по координатам Х. получается 96,96*2,96*3 и опять 96
+	//		herosprite.move(-0.1 * time, 0);//происходит само движение персонажа влево
+	//	}
 
-		if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Down) || (sf::Keyboard::isKeyPressed(sf::Keyboard::S)))) {
-			CurrentFrame += 0.005 * time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
-			if (CurrentFrame > 3) CurrentFrame -= 3; //проходимся по кадрам с первого по третий включительно. если пришли к третьему кадру - откидываемся назад.
-			herosprite.setTextureRect(sf::IntRect(96 * int(CurrentFrame), 0, 96, 96)); //проходимся по координатам Х. получается 96,96*2,96*3 и опять 96
-			herosprite.move(0, 0.1 * time);//происходит само движение персонажа вниз
-		}
+	//	if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || (sf::Keyboard::isKeyPressed(sf::Keyboard::D)))) {
+	//		CurrentFrame += 0.005 * time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
+	//		if (CurrentFrame > 3) CurrentFrame -= 3; //проходимся по кадрам с первого по третий включительно. если пришли к третьему кадру - откидываемся назад.
+	//		herosprite.setTextureRect(sf::IntRect(96 * int(CurrentFrame), 192, 96, 96)); //проходимся по координатам Х. получается 96,96*2,96*3 и опять 96
 
-		window.clear();
-		window.draw(herosprite);
-		window.display();
-	}
+	//		herosprite.move(0.1 * time, 0);//происходит само движение персонажа вправо
+
+	//	}
+
+
+	//	if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || (sf::Keyboard::isKeyPressed(sf::Keyboard::W)))) {
+	//		CurrentFrame += 0.005 * time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
+	//		if (CurrentFrame > 3) CurrentFrame -= 3; //проходимся по кадрам с первого по третий включительно. если пришли к третьему кадру - откидываемся назад.
+	//		herosprite.setTextureRect(sf::IntRect(96 * int(CurrentFrame), 288, 96, 96)); //проходимся по координатам Х. получается 96,96*2,96*3 и опять 96
+	//		herosprite.move(0, -0.1 * time);//происходит само движение персонажа вверх
+	//	}
+
+	//	if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Down) || (sf::Keyboard::isKeyPressed(sf::Keyboard::S)))) {
+	//		CurrentFrame += 0.005 * time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
+	//		if (CurrentFrame > 3) CurrentFrame -= 3; //проходимся по кадрам с первого по третий включительно. если пришли к третьему кадру - откидываемся назад.
+	//		herosprite.setTextureRect(sf::IntRect(96 * int(CurrentFrame), 0, 96, 96)); //проходимся по координатам Х. получается 96,96*2,96*3 и опять 96
+	//		herosprite.move(0, 0.1 * time);//происходит само движение персонажа вниз
+	//	}
+
+	//	window.clear();
+	//	window.draw(herosprite);
+	//	window.display();
+	//}
 
 	return 0;
 }
